fdf6f2c3287dd0c63411dccff83b7ded0898a037
hanbintan2003 <thb182003@gmail.com>
2023-03-14 19:38:07 -0700

    first

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/ice/Projects/Sort
+# Build directory: /Users/ice/Projects/Sort/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/googletest-build")
+subdirs("_deps/sfml-build")
